da07330d6f49cdfb88bb6616593dea64bd39a7b0
Laura Blanco-Berdugo <lblancob@quanah.hpcc.ttu.edu>
2018-05-02 12:07:38 -0500

    LBB_transcript.py

--- LBB_transcript.py
+++ LBB_transcript.py
@@ -5,6 +5,8 @@
 #		Number of "unigenes"
 #		Mean and median transcript length
 #		N50 explanation here
+#		calculates the # of transcripts that have blast hits
+
 
 from Bio import SeqIO
 import statistics as stat
@@ -30,19 +32,19 @@ with open('stats.txt', 'w') as STATS:
 
 	for RECORD in SeqIO.parse(INFILE, 'fasta'):
 			LEN_LIS.append(len(RECORD.seq))
-	#this calculates the total basepairs length
+	#THIS CALCULATES THE TOTAL BP LENGTH
 	print('length = ' + str(sum(LEN_LIS)) + '.')
 	STATS.write('full lengths = ' + str(sum(LEN_LIS)) + '.\n')
 	
-	#this calculates the leght of the transcript 
+	#THIS CALCULATES THE LENGTH OF THE TRANSCRIPT 
 	print ('transcripts = ' + str(len(LEN_LIS)) + '.')
 	STATS.write('transcripts = ' + str(len(LEN_LIS)) + '.\n')
 	
-	#this calculates the mean of the length of the transcript
+	#THIS CALCULATES THE MEAN OF THE LENGTH OF THE TRANSCRIPTS
 	print('mean transcripts length = ' + str(stat.mean(LEN_LIS)) + '.')
 	STATS.write('mean transcritps length = ' + str(stat.mean(LEN_LIS)) + '.\n')
 	
-	#this calculates the N50, this is the mean or median of lengths
+	#THIS CALCULATES THE N50, THIS WAS COPIED FROM DARAY'S GITHUB
 	LENLIS = sorted(LEN_LIS)
 	HALF = sum(LEN_LIS)/2
 	TOTALENTRIES = len(LEN_LIS)
@@ -54,8 +56,23 @@ with open('stats.txt', 'w') as STATS:
 			break
 	for RECORD in SeqIO.parse(INFILE, 'fasta'):
 		ID_LIS.append(RECORD.id.rsplit('_',1))
-
+	###
+	##THIS CALCULATES THE UNIGENES FROM THE FILES
+	###
 	IDS = pd.DataFrame(ID_LIS, columns = ['uni', 'iso'])
 	COUNTUNI = len(IDS['uni'].unique())
-	print('There are ' + str(COUNTUNI) + ' unigenes in the file.')
-	STATS.write('There are ' + str(COUNTUNI) + ' unigenes in the file.\n')
+	print('unigenes = ' + str(COUNTUNI))
+	STATS.write('unigenes ' + str(COUNTUNI) + '.\n')
+
+####
+##PART TWO OF THE HW. THE IDEA HERE IS TO COUNT THE SEQS THAT HAVE HITS IN THE BLAST
+###
+	#reading the file as a table and use the columns to count the hits
+	TRANSCRIPT_HITS = pd.read_table(BLASTX, sep='\t', usecols=[1,2], names=['ID', 'db'])
+	HITS = len(TRANSCRIPT_HITS.ID.unique())
+	print('UNIQUE HITS = ' + str(HITS) + '.')
+	
+#####
+##PART THREE, NUMBER OF UNIQUE PROTEINS IN THE REFERENCE PROTEOME WITH BLAST HIT
+#####
+	##count the number of times there is a hit in the blast file
